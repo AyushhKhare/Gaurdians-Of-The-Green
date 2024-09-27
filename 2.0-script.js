a3aae8076894eb6f230b65f16e9d174bacbe05a4
AyushhKhare <148583408+AyushhKhare@users.noreply.github.com>
2024-09-27 22:15:18 +0530

    Add files via upload

---
+++ 2.0-script.js
@@ -0,0 +1,21 @@
+var tl = gsap.timeline()
+
+gsap.from(".nav",{
+    y:-200,
+    duration:0.5,
+    delay:0.5,
+
+})
+
+gsap.from(".leaf img",{
+    x:-800,
+    duration:0.5,
+    delay:0.5,
+    
+})
+gsap.from(".hero .text ",{
+    x:-600,
+    duration:0.5,
+    delay:0.5,
+    
+})
